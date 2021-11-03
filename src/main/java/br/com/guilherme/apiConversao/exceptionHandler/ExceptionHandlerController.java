b174d04ab782b90fb41cedb367d2a14399a79b1b
AvelFir <guichasemartinsgmr@hotmail.com>
2021-11-03 17:23:20 -0300

    pequenas mudanças no handler

--- src/main/java/br/com/guilherme/apiConversao/exceptionHandler/ExceptionHandlerController.java
+++ src/main/java/br/com/guilherme/apiConversao/exceptionHandler/ExceptionHandlerController.java
@@ -1,6 +1,8 @@
 package br.com.guilherme.apiConversao.exceptionHandler;
 
 import feign.FeignException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,8 +13,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionHandlerController {
 
+    private final Logger logger = LoggerFactory.getLogger("Handler");
+
     @ExceptionHandler(UrlPadraoException.class)
     public ResponseEntity<ExceptionResponse> handle(UrlPadraoException e){
+        logger.error("UrlPadraoException: {}",e.getMessage());
         return new ResponseEntity<>(new ExceptionResponse(e.getMessage()),HttpStatus.NOT_ACCEPTABLE);
     }
 
@@ -20,6 +25,7 @@ public class ExceptionHandlerController {
     public ResponseEntity<Object> handleFeignStatusException(FeignException e) {
         final HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        logger.error("Feign Exception: status={} message={}",e.status(),e.contentUTF8());
         return new ResponseEntity<>(e.contentUTF8(),httpHeaders,HttpStatus.valueOf(e.status()));
     }
 
